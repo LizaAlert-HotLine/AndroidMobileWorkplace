365eb776632099523c8bac8488bada880ceb8fcf
Anton Prozorov <zlobstermeister@gmail.com>
2014-09-02 10:10:27 +0400

    Issue #5 simple form with dummy buttons

--- AndroidMobileWorkplace/src/ru/lizaalert/hotline/InputFormActivity.java
+++ AndroidMobileWorkplace/src/ru/lizaalert/hotline/InputFormActivity.java
@@ -4,14 +4,33 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
-public class InputFormActivity extends ActionBarActivity {
+public class InputFormActivity extends ActionBarActivity implements View.OnClickListener {
+
+    private EditText etPhone;
+    private EditText etCity;
+    private EditText etName;
+    private EditText etBirthday;
+    private EditText etOther;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_form);
+
+        etPhone = (EditText) findViewById(R.id.et_phone);
+        etCity = (EditText) findViewById(R.id.et_city);
+        etName = (EditText) findViewById(R.id.et_name);
+        etBirthday = (EditText) findViewById(R.id.et_birthday);
+        etOther = (EditText) findViewById(R.id.et_other);
+
+        findViewById(R.id.btn_clear).setOnClickListener(this);
+        findViewById(R.id.btn_sms).setOnClickListener(this);
+        findViewById(R.id.btn_email).setOnClickListener(this);
     }
 
 
@@ -32,5 +51,26 @@ public class InputFormActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void clearInput() {
+        etPhone.setText("");
+        etCity.setText("");
+        etName.setText("");
+        etBirthday.setText("");
+        etOther.setText("");
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_clear:
+                clearInput();
+                break;
+            case R.id.btn_sms:
+                break;
+            case R.id.btn_email:
+                break;
+        }
     }
 }
