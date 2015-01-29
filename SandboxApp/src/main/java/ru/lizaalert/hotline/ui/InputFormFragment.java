1f70163e6075441ee9c2c5c40ce30ae0dddc6d8e
Igor Tseglevskiy <igor.tseglevskiy@gmail.com>
2015-01-29 22:26:51 +0300

    Fixed #21 - Отправка через Skype

--- SandboxApp/src/main/java/ru/lizaalert/hotline/ui/InputFormFragment.java
+++ SandboxApp/src/main/java/ru/lizaalert/hotline/ui/InputFormFragment.java
@@ -62,6 +62,9 @@ package ru.lizaalert.hotline.ui;
 
 import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -187,6 +190,7 @@ public class InputFormFragment extends Fragment implements View.OnClickListener,
         findViewById(R.id.btn_sms).setOnClickListener(this);
         findViewById(R.id.btn_email).setOnClickListener(this);
         findViewById(R.id.btn_vk).setOnClickListener(this);
+        findViewById(R.id.btn_skype).setOnClickListener(this);
     }
 
     @Override
@@ -233,6 +237,16 @@ public class InputFormFragment extends Fragment implements View.OnClickListener,
                 break;
             case R.id.btn_vk:
                 vkManager.requestWallPost(composeMessage());
+                break;
+            case R.id.btn_skype:
+                ClipboardManager clipboard = (ClipboardManager) getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("8800", composeMessage());
+                clipboard.setPrimaryClip(clip);
+
+                Intent skype_intent = new Intent("android.intent.action.VIEW");
+                skype_intent.setClassName("com.skype.raider", "com.skype.raider.Main");
+                skype_intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(skype_intent);
                 break;
         }
     }
